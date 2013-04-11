5dbd4bbbb7eff191fca43499388ac2d17f2a438c
funnellw <will.funnell@lbi.com>
2013-04-11 11:23:34 +0100

    deleted cake stuff

--- src/main/scala/com/lbi/scalatra/domain/PotSensor.scala
+++
@@ -1,9 +0,0 @@
-package com.lbi.scalatra.domain
-
-trait SensorDeviceComponentImpl extends SensorDeviceComponent {
-
-  class PotSensor extends SensorDevice {
-    def isCoffeePresent = true
-  }
-
-}
